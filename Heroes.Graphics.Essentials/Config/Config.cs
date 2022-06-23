4369628d4c7c9e3559b6c28c355216da7fdad9c1
Sewer 56 <seweratwork@gmail.com>
2022-06-23 16:02:03 +0100

    Improved: Settings Menu User Experience

--- Heroes.Graphics.Essentials/Config/Config.cs
+++ Heroes.Graphics.Essentials/Config/Config.cs
@@ -8,42 +8,52 @@ public class Config : Configurable<Config>
 {
     [DisplayName("Width")]
     [Description("Width of the game window, in pixels")]
+    [DefaultValue(1280)]
     public int  Width                       { get; set; } = 1280;
 
     [DisplayName("Height")]
     [Description("Height of the game window, in pixels")]
+    [DefaultValue(720)]
     public int  Height                      { get; set; } = 720;
 
     [DisplayName("Borderless Windowed")]
     [Description("If set to true and game is in windowed mode, the window will have no border.")]
+    [DefaultValue(true)]
     public bool BorderlessWindowed          { get; set; } = true;
 
     [DisplayName("Resizable Window")]
     [Description("If set to true, the game window can be resized.")]
+    [DefaultValue(false)]
     public bool ResizableWindowed           { get; set; } = false;
 
     [DisplayName("High Aspect Ratio Crashfix")]
     [Description("Workaround for borderless windowed that prevents the game crashing on titlecards if the aspect ratio of the game is ridiculous.")]
+    [DefaultValue(true)]
     public bool HighAspectRatioCrashFix     { get; set; } = true;
 
     [DisplayName("Aspect Ratio Limit")]
     [Description("If the game window is below this aspect ratio, the widescreen hack scales the window vertically instead of horizontally.")]
+    [DefaultValue((float)(4.0F / 3.0F))]
     public float AspectRatioLimit           { get; set; } = 4 / 3F;
 
     [DisplayName("Disable 2P Frameskip")]
     [Description("Disables the frame skipping behaviour in 2 player mode, allowing you to play at 60 FPS.")]
+    [DefaultValue(true)]
     public bool Disable2PFrameskip          { get; set; } = true;
 
     [DisplayName("Fast Stage Load Times")]
     [Description("A hack that skips waiting in titlecards, causing levels to load instantly.")]
+    [DefaultValue(true)]
     public bool StupidlyFastLoadTimes       { get; set; } = true;
 
     [DisplayName("Do Not Slowdown on Focus Loss")]
     [Description("Stops the game from slowing down when focus is lost.")]
+    [DefaultValue(true)]
     public bool DontSlowdownOnFocusLost { get; set; } = true;
 
     [DisplayName("Center Window to Screen")]
     [Description("Moves the window to the center of the screen.")]
+    [DefaultValue(true)]
     public bool CenterWindow { get; set; } = true;
 
     [DisplayName("Default Settings")]
--- Heroes.Graphics.Essentials/Heroes/DefaultSettings.cs
+++ Heroes.Graphics.Essentials/Heroes/DefaultSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using Heroes.SDK.API;
 using Heroes.SDK.Definitions.Enums;
 
@@ -10,16 +11,58 @@ namespace Heroes.Graphics.Essentials.Heroes;
 public unsafe class DefaultSettings
 {
     // Configuration fields.
+
+    [DisplayName("Full Screen")]
+    [Description("Makes the game run in exclusive fullscreen mode.")]
+    [DefaultValue(false)]
     public bool Fullscreen              { get; set; } = false;
+
+    [DisplayName("Language")]
+    [Description("Default language for the game. Overwritten by save file.")]
+    [DefaultValue(Language.English)]
     public Language Language            { get; set; } = Language.English;
+
+    [DisplayName("Sound FX Volume")]
+    [Description("Volume for sound effects. Between 0 and 100.")]
+    [DefaultValue((byte)100)]
     public byte SFXVolume               { get; set; } = 100;
+
+    [DisplayName("Music Volume")]
+    [Description("Volume for music. Between 0 and 100.")]
+    [DefaultValue((byte)100)]
     public byte BGMVolume               { get; set; } = 100;
+
+    [DisplayName("3D Sound")]
+    [Description("Basically Surround Sound.")]
+    [DefaultValue(true)]
     public bool ThreeDimensionalSound   { get; set; } = true;
+
+    [DisplayName("Enable Sound Effects")]
+    [DefaultValue(true)]
     public bool SFXOn                   { get; set; } = true;
+
+    [DisplayName("Enable Music")]
+    [DefaultValue(true)]
     public bool BGMOn                   { get; set; } = true;
+
+    [DisplayName("Soft Shadows")]
+    [Description("When enabled, shadow models are accurate to the characters/enemies.\n" +
+                 "When disabled, uses circles for shadows.")]
+    [DefaultValue(true)]
     public bool SoftShadows             { get; set; } = true;
+
+    [DisplayName("Enable Mouse Control")]
+    [Description("Setting intended for Masochists")]
+    [DefaultValue(-1)]
     public int  MouseControl            { get; set; } = -1;
+
+    [DisplayName("Disable Character Dialogue")]
+    [Description("Disables certain character dialogue.\n" +
+                 "Internally known as CharmyShutup in game code.")]
+    [DefaultValue(false)]
     public bool CharmyShutup            { get; set; } = false;
+
+    [DefaultValue(true)]
     public bool Fog                     { get; set; } = true;
 
     /// <summary>
